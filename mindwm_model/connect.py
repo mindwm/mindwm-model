bdb3bd847666a05a29bd1a27f4b0911fe1b5bd85
Your Name <you@example.com>
2024-04-03 07:51:50 +0000

    fix

--- mindwm_model/connect.py
+++ mindwm_model/connect.py
@@ -2,6 +2,6 @@ from neo4j import GraphDatabase
 from neomodel import config
 
 class Neo4j:
-    __init__(self, url, username, password):
+    def __init__(self, url, username, password):
         config.DRIVER = GraphDatabase().driver(url, auth=(username, password))
         self.driver = config.DRIVER
